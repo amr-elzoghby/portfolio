fa69d0d0fdfe965fd27049257a16aeba7a1dfaec
Amr Elzoghby <amr.s.elzoghby@gmail.com>
2026-04-23 08:16:01 +0200

    Fix outputs reference

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,5 @@
 # infra/outputs.tf
 
-output "s3_website_url" {
-  value = aws_s3_bucket_website_configuration.portfolio.website_endpoint
-}
-
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.portfolio.domain_name
 }
