88c38bd88e2d2590fe8af282722bca68a77f8c02
Amr Elzoghby <amr.s.elzoghby@gmail.com>
2026-04-23 08:13:59 +0200

    Handle existing S3 bucket elegantly

--- infra/main.tf
+++ infra/main.tf
@@ -13,48 +13,16 @@ provider "aws" {
   region = var.aws_region
 }
 
-# 1. S3 Bucket for Hosting
-resource "aws_s3_bucket" "portfolio" {
-  bucket        = var.bucket_name
-  force_destroy = true
-}
 
-resource "aws_s3_bucket_public_access_block" "portfolio" {
-  bucket = aws_s3_bucket.portfolio.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_website_configuration" "portfolio" {
-  bucket = aws_s3_bucket.portfolio.id
-  index_document { suffix = "index.html" }
-}
-
-resource "aws_s3_bucket_policy" "public_read" {
-  depends_on = [aws_s3_bucket_public_access_block.portfolio]
-  bucket     = aws_s3_bucket.portfolio.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.portfolio.arn}/*"
-      },
-    ]
-  })
+data "aws_s3_bucket" "portfolio" {
+  bucket = var.bucket_name
 }
 
 # 2. CloudFront Distribution
 resource "aws_cloudfront_distribution" "portfolio" {
   origin {
-    domain_name = aws_s3_bucket.portfolio.bucket_regional_domain_name
-    origin_id   = "S3-${aws_s3_bucket.portfolio.bucket}"
+    domain_name = data.aws_s3_bucket.portfolio.bucket_regional_domain_name
+    origin_id   = "S3-${var.bucket_name}"
   }
 
   enabled             = true
@@ -64,7 +32,7 @@ resource "aws_cloudfront_distribution" "portfolio" {
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-${aws_s3_bucket.portfolio.bucket}"
+    target_origin_id = "S3-${var.bucket_name}"
 
     forwarded_values {
       query_string = false
